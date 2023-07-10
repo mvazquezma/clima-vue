07bc04607b92a114086d1f3508f17ff1ed845962
mvazquezma <mvm51090@gmail.com>
2023-07-10 16:49:27 +0200

    Form created, and error, and alert

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <script setup>
+import Formulario from './components/Formulario.vue';
+
 </script>
 
 <template>
+  <h1 class="titulo">Buscador de Clima</h1>
 
+  <div class="contenedor buscador-clima">
+    <Formulario />
+  </div>
+  
 </template>
