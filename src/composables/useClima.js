2897c1f2fa22aa7ca5255b0b668df7f6c47b1101
mvazquezma <mvm51090@gmail.com>
2023-07-10 17:40:00 +0200

    Added api key and doing axios petition

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -1,8 +1,28 @@
+import axios from 'axios';
+
 export default function useClima() {
 
-    const obtenerClima = ({ciudad, pais}) => {
-        console.log('consultando..', ciudad);
-        console.log('consultando..', pais);
+    const obtenerClima = async ({ciudad, pais}) => {
+        // importar el Api key
+        const key = import.meta.env.VITE_API_KEY
+
+        try {
+            // Obtener la latitud, longitud
+            const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
+            const {data} = await axios(url)
+            const {lat, lon} = data[0]
+
+            // Obtener clima
+            const urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`
+            const {data: resultado} =  await axios(urlClima)
+            console.log(resultado);
+
+        } catch (error) {
+            console.error(error);
+        }
+       
+
+        // Obtener el clima
     }
 
     return {
